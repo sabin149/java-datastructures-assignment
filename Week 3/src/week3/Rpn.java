83894fc61af66a9748b449ea65d0e9db64c0b2e3
sabin149 <58678988+sabin149@users.noreply.github.com>
2021-04-06 13:44:05 +0545

    Week 3

---
+++ Week 3/src/week3/Rpn.java
@@ -0,0 +1,78 @@
+package week3;
+
+import java.util.*;
+
+
+import java.util.*;
+public class Rpn {
+     public int stacky(String[] tokens) {
+        
+            
+            Stack<String> stack = new Stack<String>(); 
+            int x, y; 
+            String result = ""; 
+            int get = 0; 
+            String choice; 
+            int value = 0; 
+            String p = ""; 
+      
+      
+            for (int i = 0; i < tokens.length; i++) { 
+                if (!tokens[i].equals("+") && !tokens[i].equals("-")
+                    && !tokens[i].equals("*") && !tokens[i].equals("/")) { 
+                    stack.push(tokens[i]); 
+                    continue; 
+                } 
+                else { 
+                    choice = tokens[i]; 
+                } 
+      
+             
+                switch (choice) { 
+                
+                case "+": 
+      
+                    x = Integer.parseInt(stack.pop()); 
+                    y = Integer.parseInt(stack.pop()); 
+                    value = x + y; 
+                    result = p + value; 
+                    stack.push(result); 
+                    break; 
+      
+                case "-": 
+                    x = Integer.parseInt(stack.pop()); 
+                    y = Integer.parseInt(stack.pop()); 
+                    value = x - y; 
+                    result = p + value; 
+                    stack.push(result); 
+                    break; 
+      
+                case "*": 
+                    x = Integer.parseInt(stack.pop()); 
+                    y = Integer.parseInt(stack.pop()); 
+                    value = x * y; 
+                    result = p + value; 
+                    stack.push(result); 
+                    break; 
+      
+                case "/":  
+      
+                    x = Integer.parseInt(stack.pop()); 
+                    y = Integer.parseInt(stack.pop()); 
+                    value = y / x; 
+                    result = p + value; 
+                    stack.push(result); 
+                    break; 
+      
+                default: 
+                    continue; 
+                } 
+            } 
+      
+        
+            return Integer.parseInt(stack.pop()); 
+        } 
+
+ 
+
+}
